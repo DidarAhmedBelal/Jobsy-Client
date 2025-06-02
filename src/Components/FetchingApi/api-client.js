805574d4f355db8d0ed16f046ad227bba10c36b4
Belal Islam <belalislam294@gmail.com>
2025-06-03 01:00:50 +0600

    checkt

--- src/Components/FetchingApi/api-client.js
+++ src/Components/FetchingApi/api-client.js
@@ -1,13 +1,13 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://job-sy.vercel.app/api/v1",
-});
-
-
-
 // import axios from "axios";
 
 // export default axios.create({
-//   baseURL: "http://127.0.0.1:8000/api/v1",
+//   baseURL: "https://job-sy.vercel.app/api/v1",
 // });
+
+
+
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "http://127.0.0.1:8000/api/v1",
+});
